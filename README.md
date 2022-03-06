6fda7d0a79670948913ce69a5d6020f85e43de5e
Oussama Messabih <oussama@swissdigilab.com>
2022-03-06 01:07:35 +0100

    adding cli

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # json-translator-cli
+simple cli to translate your json i18n files
+
+1. GET api key from: [Rapid api ms translator](https://rapidapi.com/microsoft-azure-org-microsoft-cognitive-services/api/microsoft-translator-text/)
+2.  `pip install -r requirements.txt`
+3.  `python translate_cli.py -h`
+
+### Usage:
+`python translate_cli.py -i <inputfile> --li=<input language> -o <outputfile> --lo=<output language>`
+
+### Example:
+
+`python translate_cli.py -i common.json -o common_fr.json --li=en --lo=fr`
+
+### Contributing
+Please feel free to fork this package and contribute by submitting a pull request to enhance the functionalities.
---
+++ translate_cli.py
@@ -0,0 +1,70 @@
+import requests
+import sys, getopt
+import json
+
+def translate(input, inputlanguage, outputlanguage):
+    
+    url = "https://microsoft-translator-text.p.rapidapi.com/translate"
+
+    querystring = {"api-version":"3.0","from": f"{inputlanguage}","to":f"{outputlanguage}","textType":"plain","profanityAction":"NoAction"}
+
+    payload = [{"Text": input}]
+    payload = json.dumps(payload, indent=4)
+    headers = {
+        'content-type': "application/json",
+        'x-rapidapi-host': "microsoft-translator-text.p.rapidapi.com",
+        'x-rapidapi-key': "0c04854be6mshdf9a5de23527865p1472e3jsn4c2ce93c1a9e"
+        }
+
+    response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
+    resJson = response.json()
+
+    print("Translation: {} -> {}".format(input, resJson[0]['translations'][0]['text']))
+    return resJson[0]['translations'][0]['text']
+
+def translate_file(input_file, inputlanguage, output_file, outputlanguage):
+    with open(input_file) as json_file:
+        data = json.load(json_file)
+        translated_data = data.copy()
+        for key in data:
+            if isinstance(data[key], str):
+                translated_data[key] = translate(data[key], inputlanguage, outputlanguage)
+        print(translated_data)
+        with open(output_file, 'w') as output_json_file:
+            json_text = json.dumps(translated_data, indent=2)
+            output_json_file.write(json_text)
+
+def main(argv):
+    print('translate_cli.py -i <inputfile> -li <input language> -o <outputfile> -lo <output language>')
+    inputfile = ''
+    outputfile = ''
+    languageinput = ''
+    languageoutput = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile=", "li=", "lo="])
+    except getopt.GetoptError:
+        print('translate_cli.py -i <inputfile> --li=<input language> -o <outputfile> --lo=<output language>')
+        sys.exit(2)
+    print(opts)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('translate_cli.py -i <inputfile> --li=<input language> -o <outputfile> --lo=<output language>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+        elif opt in ("--li"):
+            languageinput = arg
+        elif opt in ("--lo"):
+            languageoutput = arg
+    print('Input file is ', inputfile)
+    print('Input language is ', languageinput)
+    print('Output file is ', outputfile)
+    print('Output languageoutput is ', languageoutput)
+    if inputfile and languageinput and languageoutput and outputfile:
+        print("Translating...")
+        translate_file(inputfile, languageinput, outputfile, languageoutput)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
